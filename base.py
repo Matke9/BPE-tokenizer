2ccaf02b92d1bbc911a13bf771b4212559d50653
Mihailo Matović <64710388+Matke9@users.noreply.github.com>
2026-04-25 16:21:16 +0200

    save and load methods implemented, merge method removed

--- base.py
+++ base.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod, ABC
 
+import json
+
 
 class AbstractTokenizer(ABC):
     def __init__(self):
@@ -16,10 +18,6 @@ class AbstractTokenizer(ABC):
     def train(self, training_data):
         pass
 
-    @abstractmethod
-    def merge(self, token1, token2):
-        pass
-
     def decode(self, tokens):
         text = ''
         for token in tokens:
@@ -29,3 +27,22 @@ class AbstractTokenizer(ABC):
                 text += token
         text = text.replace('Ġ', ' ')
         return text
+
+    def save_vocab_and_merges(self, vocab_path, merges_path):
+        with open(vocab_path, "w", encoding="utf-8") as file:
+            json.dump(self.vocab, file, ensure_ascii=False, indent=2)
+        with open(merges_path, "w", encoding="utf-8") as file:
+            merges_list = [{"pair": list(pair), "new_id": new_id}
+                           for pair, new_id in self.bpe_merges.items()]
+            json.dump(merges_list, file, ensure_ascii=False, indent=2)
+
+    def load_vocab_and_merges(self, vocab_path, merges_path):
+        with open(vocab_path, "r", encoding="utf-8") as file:
+            loaded_vocab = json.load(file)
+            self.vocab = {int(k): v for k, v in loaded_vocab.items() }
+            self.inv_vocab = {v: int(k) for k, v in loaded_vocab.items() }
+        with open(merges_path, "r", encoding="utf-8") as file:
+            loaded_merges = json.load(file)
+            for merge in loaded_merges:
+                pair = tuple(merge["pair"])
+                self.bpe_merges[pair] = merge["new_id"]
