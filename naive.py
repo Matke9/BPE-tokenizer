f76d13ed7b5351d497cf731b375a71d4071900c7
Mihailo Matović <64710388+Matke9@users.noreply.github.com>
2026-04-28 21:04:09 +0200

    created a empty naive.py

---
+++ naive.py
@@ -0,0 +1,12 @@
+from base import AbstractTokenizer
+
+
+class NaiveTokenizer(AbstractTokenizer):
+    def __init__(self):
+        super().__init__()
+
+    def encode(self, text):
+        pass
+
+    def train(self, training_data):
+        pass
